675df7596c2871a68db447ea215632c4c48aba66
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-02-26 16:44:30 +0100

    feat: add functionality for logging in users

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,8 +1,26 @@
 import Logo from '../assets/media/logo.svg'
 import BigImg from '../assets/media/hero--img.png'
 import '../styles/login.page.scss'
+import { useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+  const passwordFieldRef = useRef<HTMLInputElement>(null)
+  const defaultUserInfo = {
+    email: '',
+    password: '',
+  }
+  const navigate = useNavigate()
+  const [userInfo, setuserInfo] = useState(defaultUserInfo)
+  const [passwordVisibility, setPasswordVisibility] = useState(false)
+  
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    navigate("/dashboard")
+    setuserInfo(defaultUserInfo)
+  }
+
   return (
     <div className='page__control login__page'>
       <div>
@@ -15,19 +33,19 @@ const Login = () => {
           </div>
         </div>
         <main>
-          <form>
+          <form onSubmit={e => onSubmit(e)}>
             <div>
               <h1>Welcome!</h1>
               <p>Enter details to login.</p>
             </div>
             <div>
               <div className="field__control">
-                <input type="email" name="email__field" id="email__field" placeholder='Email' required />
+                <input onChange={e => setuserInfo({...userInfo, email: e.target.value})} value={userInfo.email} type="email" name="email__field" id="email__field" placeholder='Email' required />
               </div>
               <div className="field__control">
                 <div className="pass--field__box">
-                  <input type="password" name="password__field" id="password__field" placeholder='Password' required />
-                  <span>SHOW</span>
+                  <input onChange={e => setuserInfo({...userInfo, password: e.target.value})} value={userInfo.password} type={passwordVisibility === true ? 'text': 'password'} name="password__field" id="password__field" ref={passwordFieldRef} placeholder='Password' required />
+                  <span onClick={() => setPasswordVisibility(!passwordVisibility)}>SHOW</span>
                 </div>
                 <small>FORGOT PASSWORD?</small>
               </div>
