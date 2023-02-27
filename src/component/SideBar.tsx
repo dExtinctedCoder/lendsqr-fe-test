12f89bd54353d4e58ba3a63f24509aaa462acd93
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-02-28 00:03:32 +0100

    feat: create full markup and logic of sidebar component

--- src/component/SideBar.tsx
+++ src/component/SideBar.tsx
@@ -1,6 +1,73 @@
+import {useContext, Fragment} from 'react'
+import {FiChevronDown} from 'react-icons/Fi'
+import { sideBarValues } from "../assets/app-data/SideBarContent"
+import { AppContext } from "../assets/context/AppContext"
+import '../styles/side-bar.scss'
+
 const SideBar = () => {
+  const {isMenuOpen, navHeight} = useContext(AppContext)
+  const sideBarStyle: React.CSSProperties = {
+    transition: 'all 350ms',
+    transform: isMenuOpen ? 'translateX(0vw)' : 'translateX(-100vw)',
+    maxHeight: `calc(100vh - ${navHeight}px)`
+  }
+
   return (
-    <div>SideBar</div>
+    <Fragment>
+      <aside style={{maxHeight: `calc(100vh - ${navHeight}px)`}} className="desktop--mode__control">
+        <SideBarJsx />
+      </aside>
+      <aside style={sideBarStyle} className="mobile--mode__control">
+        <SideBarJsx />
+      </aside>
+    </Fragment>
+  )
+}
+
+export const SideBarJsx =() => {
+  return (
+    <div className='side--bar'>
+      <h4>{sideBarValues.SWITCHORGICON} <span>Switch Organization</span><FiChevronDown fontSize={16} color='#213F7D' /></h4>
+      <ul>
+        <li>{sideBarValues.DASHBOARDICON} <span>Dashboard</span></li>
+        <li>
+          <ul>
+            <h5>CUSTOMERS</h5>
+            {
+              sideBarValues.CUSTOMERS.map(({Icon, tittle}) => {
+                return (
+                  <li key={tittle}>{Icon} <span>{tittle}</span></li>
+                )
+              })
+            }
+          </ul>
+        </li>
+        <li>
+          <ul>
+            <h5>BUSINESSES</h5>
+            {
+              sideBarValues.BUSINESSES.map(({Icon, tittle}) => {
+                return (
+                  <li key={tittle}>{Icon} <span>{tittle}</span></li>
+                )
+              })
+            }
+          </ul>
+        </li>
+        <li>
+          <ul>
+            <h5>SETTINGS</h5>
+            {
+              sideBarValues.SETTINGS.map(({Icon, tittle}) => {
+                return (
+                  <li key={tittle}>{Icon} <span>{tittle}</span></li>
+                )
+              })
+            }
+          </ul>
+        </li>
+      </ul>
+    </div>
   )
 }
 
