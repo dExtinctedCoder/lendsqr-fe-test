d464115d6665639d348395609d86a8c24e1d3819
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-02-27 10:57:38 +0100

    feat: add Nav.tsx component to Dashboard

--- src/pages/Dashboard.page.tsx
+++ src/pages/Dashboard.page.tsx
@@ -1,6 +1,17 @@
+import {useContext} from 'react'
+import { AppContext } from '../assets/context/AppContext'
+import Nav from "../component/Nav"
+import SideBar from "../component/SideBar"
+
 const Dashboard = () => {
+
+  const {isMenuOpen} = useContext(AppContext)
   return (
-    <div>Dashboard</div>
+    <div>
+      <Nav />
+      {isMenuOpen && <SideBar />}
+      <h1>Dashboard!</h1>
+    </div>
   )
 }
 
