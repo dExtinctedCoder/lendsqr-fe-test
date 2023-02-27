a51d4338171f90b7d6576fe838d934c6d639c8da
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-02-27 11:00:44 +0100

    ci: add type definition for AppContext component

--- src/types/context.type.tsx
+++ src/types/context.type.tsx
@@ -8,3 +8,12 @@ export type AuthContextType = {
 export type AuthContextProviderPropsType = {
   children: React.ReactNode
 }
+
+export type AppContextType = {
+  isMenuOpen: boolean
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export type AppContextProviderPropsType = {
+  children: React.ReactNode
+}
