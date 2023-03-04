2329152eb0edfba735619b69559a32d465a8555a
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-04 06:59:58 +0100

    build: create loading component to indicate loading state

---
+++ src/component/Loading.tsx
@@ -0,0 +1,20 @@
+import '../styles/_utilities.scss'
+
+const Loader = () => {
+  return (
+    <div className='loading__box'>
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
